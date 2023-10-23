971b5407f20101d50079348082fe8ca75d22f57a
XaviaThe2nd <xaviathesecond@gmail.com>
2023-10-23 12:10:26 +0800

    added some methods to register for camp in student

--- CAMs App/src/CAMs_App/Student.java
+++ CAMs App/src/CAMs_App/Student.java
@@ -1,5 +1,35 @@
 package CAMs_App;
 
-public class Student {
+import java.util.ArrayList;
 
+public class Student extends User {
+	//User values
+	//UserID
+	//password
+	//faculty
+	
+	private ArrayList<Camp> attending = new ArrayList<>();
+	private Camp comitteeCamp = null;
+	
+	public void registerCamp (Camp camp, boolean comittee) {
+		if(comittee) {
+			comitteeCamp = camp;
+			attending.add(camp);
+		} else {
+			attending.add(camp);
+		}
+	}
+	
+	public boolean isComittee () {
+		return comitteeCamp != null ? true : false;
+	}
+	
+	public boolean checkDateClash (Camp camp) {
+		for(int i = 0; i < attending.size(); i++) {
+			if (attending(i).date == camp.date) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
