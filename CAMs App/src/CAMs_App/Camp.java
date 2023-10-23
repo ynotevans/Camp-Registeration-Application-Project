9d6663e25d85295c026ad6166af463021e9a6874
Tony <123456789@user.noreply.gmail.com>
2023-10-23 15:05:48 +0800

    added extend to campInfo

--- CAMs App/src/CAMs_App/Camp.java
+++ CAMs App/src/CAMs_App/Camp.java
@@ -1,4 +1,14 @@
 package CAMs_App;
 
 public class Camp{
+    private CampInfo campInfo;
+    public Camp(){
+        campInfo = new CampInfo(null, 0, 0, null, 0, 0, null, null);
+
+    }
+
+    public CampInfo getCampInfo(){
+        return campInfo;
+    }
+
 }
