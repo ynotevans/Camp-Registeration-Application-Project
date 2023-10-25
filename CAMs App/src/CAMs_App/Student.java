8445e3dcbf816eafaf60b123d6b97f43ce3ccf3b
XaviaThe2nd <xaviathesecond@gmail.com>
2023-10-25 15:55:22 +0800

    updated student class

--- CAMs App/src/CAMs_App/Student.java
+++ CAMs App/src/CAMs_App/Student.java
@@ -3,10 +3,11 @@ package CAMs_App;
 import java.util.ArrayList;
 
 public class Student extends User {
-	//User values
-	//UserID
-	//password
-	//faculty
+	
+	public Student(int userID, String password, String faculty) {
+		super(userID, password, faculty);
+		// TODO Auto-generated constructor stub
+	}
 	
 	private ArrayList<Camp> attending = new ArrayList<>();
 	private Camp comitteeCamp = null;
@@ -20,16 +21,36 @@ public class Student extends User {
 		}
 	}
 	
+	public void withdrawCamp (Camp camp) {
+		for(int i = 0; i < attending.size(); i++) {
+			if (attending.get(i) == camp) {
+				attending.remove(i);
+				//TODO ban list
+				comitteeCamp = comitteeCamp == camp ? null : comitteeCamp;
+				break;
+			}
+		}
+		System.out.println("Student is not registered to this camp.");
+	}
+	
 	public boolean isComittee () {
 		return comitteeCamp != null ? true : false;
 	}
 	
 	public boolean checkDateClash (Camp camp) {
 		for(int i = 0; i < attending.size(); i++) {
-			if (attending(i).date == camp.date) {
+			if (attending.get(i).getCampInfo().getCampDate() == camp.getCampInfo().getCampDate()) {
 				return true;
 			}
 		}
 		return false;
+	}
+	
+	public ArrayList<Camp> getAttendingList () {
+		return attending;
+	}
+	
+	public Camp getComitteeCamp () {
+		return comitteeCamp;
 	}
 }
