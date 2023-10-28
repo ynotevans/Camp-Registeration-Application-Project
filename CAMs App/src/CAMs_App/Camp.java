8be4d0bddf59fed5778f14d1d8a09b7560657062
Tony <123456789@user.noreply.gmail.com>
2023-10-28 20:14:47 +0800

    added array list into camp class

--- CAMs App/src/CAMs_App/Camp.java
+++ CAMs App/src/CAMs_App/Camp.java
@@ -1,7 +1,15 @@
 package CAMs_App;
 
+import java.util.ArrayList;
+
 public class Camp{
+
+    private ArrayList<Student> attendees = new ArrayList<String>();
+    private ArrayList<Student> committee = new ArrayList<String>();
+    private ArrayList<Enquiries> enquiries = new ArrayList<String>();
+    private ArrayList<Suggestion> suggestions = new ArrayList<String>();
     private CampInfo campInfo;
+    
     public Camp(){
         campInfo = new CampInfo(null, 0, 0, null, 0, null, null, null);
     }
