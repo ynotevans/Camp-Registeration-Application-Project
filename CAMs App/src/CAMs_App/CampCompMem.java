eb1951744d04cde08c55e5800a3c3dfe2f635a65
Tony <123456789@user.noreply.gmail.com>
2023-10-28 20:29:29 +0800

    init parameters and functions

--- CAMs App/src/CAMs_App/CampCompMem.java
+++ CAMs App/src/CAMs_App/CampCompMem.java
@@ -1,5 +1,21 @@
 package CAMs_App;
 
-public class CampCompMem {
+import java.util.ArrayList;
 
+public class CampCompMem {
+    private ArrayList<String> suggestions = new ArrayList<String>();
+    
+    public void viewCampDetails(){};
+
+    public void viewEnquiry(){};
+    
+    public void replyEnquiry(){};
+    
+    public void submitSuggestion(){};
+    
+    public void viewSuggestion(){};
+    
+    public void editSuggestions(){};
+    
+    public void deleteSuggestion(){};
 }
