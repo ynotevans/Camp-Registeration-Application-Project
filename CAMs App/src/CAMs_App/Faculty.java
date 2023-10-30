3678a6f891609761c66dd0397c438e4781bbbe2a
Tony <123456789@user.noreply.gmail.com>
2023-10-30 18:20:33 +0800

    added enum class to faculty

--- CAMs App/src/CAMs_App/Faculty.java
+++ CAMs App/src/CAMs_App/Faculty.java
@@ -1,5 +1,9 @@
 package CAMs_App;
 
 public class Faculty{
-    
+    public enum Faculty{
+        NIE, NBS, EEE, SOH, MAE, SCSE,
+        SPMS, SSS, CCEB, LKC, SBS, CEE,
+        MSE, WKWSOC, ADM, ASE, SCBE;
+    };
 }
