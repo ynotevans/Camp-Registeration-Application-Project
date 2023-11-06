8b26409c7f06ed78205084d69e8d3a3c04118fa1
LMeng18 <121382376+LMeng18@users.noreply.github.com>
2023-11-06 11:46:39 +0800

    added changedpassword( )

--- CAMs App/src/CAMs_App/User.java
+++ CAMs App/src/CAMs_App/User.java
@@ -1,8 +1,11 @@
 package CAMs_App;
 
 import CAMs_App.enums.*;
+import java.util.Scanner;
 
 public class User {
+     Scanner scan = new Scanner(System.in);
+
     private int userID;       //UserID should be String?
     private String password;
     private Status status;
@@ -21,7 +24,7 @@ public class User {
         this.userID = userID;
     }
 
-    public void setPassword(String password) {
+    private void setPassword(String password) {
         this.password = password;
     }
 
@@ -40,6 +43,31 @@ public class User {
     public String getFaculty() {
         return faculty.toString();
     }
-    
-    
+
+    public void changePassword(){
+        System.out.println("Changing password...");
+        System.out.println("Enter old password: ");
+        String oldPwd = scan.next();
+        if(oldPwd == this.getPassword()){
+             System.out.println("New password: ");
+             String newPwd1 = scan.next();
+
+             System.out.println("Confirm new password");
+             String newPwd2 = scan.next();
+             
+             while(newPwd1 != newPwd2){
+                System.out.println("Password deosn't match. Reenter your new password");
+                System.out.println("New Password"); 
+                newPwd1 = scan.next();
+                System.out.println("Confirm Password: ");
+                newPwd2 = scan.next();
+                }
+            
+            setPassword(newPwd2);
+            System.out.println("Password updated..");
+        }
+        else{
+            System.out.println("Wrong password!!Attempt failed");
+        }
+    }      
 }
