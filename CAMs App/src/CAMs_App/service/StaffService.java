beb4ee6809977ba895642f57d601b279ef994d09
Tony <123456789@user.noreply.gmail.com>
2023-11-09 14:23:02 +0800

    put req for staff svc

---
+++ CAMs App/src/CAMs_App/service/StaffService.java
@@ -0,0 +1,13 @@
+package CAMs_App.service;
+
+public class StaffService {
+    // view camp info
+    // create, edit, delete camp
+    // toggle visibility
+    // view camp
+    // see camps created
+    // staff can view and reply to the students in the camp she created
+    // can view and approve changes
+    // generate attendance report
+    // generate performance report
+}
