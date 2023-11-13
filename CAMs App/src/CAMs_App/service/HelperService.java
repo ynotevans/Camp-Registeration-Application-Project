a510aaf066822964f33d99de99362ca2460992f5
WJ-MJ <115694501+WJ-MJ@users.noreply.github.com>
2023-11-14 01:08:10 +0800

    added methods

--- CAMs App/src/CAMs_App/service/HelperService.java
+++ CAMs App/src/CAMs_App/service/HelperService.java
@@ -1,27 +1,18 @@
 package CAMs_App.service;
-import CAMs_App.data.Database;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
-import CAMs_App.entity.Camp;
-import CAMs_App.entity.Student;
-import CAMs_App.entity.CampCompMem;
-import CAMs_App.entity.Suggestions;
-import CAMs_App.enums.Faculty;
-import CAMs_App.entity.Enquiries;
 
 // class provides methods for reading and writing data from / to CSV files 
 
 public class HelperService {
-   
 
-
+/**
+   * Method to clear the screen of the terminal for user experience and neat
+   * interface.
+   */
+  public static void clearScreen() {
+    try {
+      new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+    } catch (Exception err) {}
+  }
 
 }
