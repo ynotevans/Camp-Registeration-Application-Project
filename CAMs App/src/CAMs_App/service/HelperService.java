9e2d8f2c0cd4fb148bc28cf5f39901ee5fb59b49
WJ-MJ <115694501+WJ-MJ@users.noreply.github.com>
2023-11-13 22:03:05 +0800

    added some helper functions

--- CAMs App/src/CAMs_App/service/HelperService.java
+++ CAMs App/src/CAMs_App/service/HelperService.java
@@ -103,5 +103,17 @@ public class HelperService {
     }
 
 
+     /**
+     * Method to clear the screen of the terminal for user experience and neat
+     * interface.
+     */
+    public static void clearScreen() {
+        try {
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        } catch (Exception err) {}
+    }
+
+
+
 
 }
