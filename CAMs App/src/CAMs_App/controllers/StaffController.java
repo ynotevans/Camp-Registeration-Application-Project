bb02101d502b1635b6d8ec84feb356efe0be8352
WJ-MJ <115694501+WJ-MJ@users.noreply.github.com>
2023-11-14 01:07:54 +0800

    added staff controller methods

--- CAMs App/src/CAMs_App/controllers/StaffController.java
+++ CAMs App/src/CAMs_App/controllers/StaffController.java
@@ -1,4 +1,59 @@
 package CAMs_App.controllers;
 
 public class StaffController extends UserController{
+    public void createCamp(){
+
+    }
+
+    public void editCamp(){
+
+    }
+
+    public void deleteCamp(){
+
+    }
+
+    public void toggleVisibility(){
+
+    }
+
+    public void viewAllCamp(){
+        System.out.println("print all camp........");
+    }
+
+    public void viewCreatedCamp(String staffID){
+        System.out.println("print staff created camp........");
+    }
+
+    public void generateReport(String campName){
+
+    }
+
+    public void viewCommitteeList(String campName){
+
+    }
+
+    public void viewAttendeeList(String campName){
+
+    }
+
+    public void viewSuggestions(){
+
+    }
+
+    public void processSuggestions(){
+
+    }
+
+    public void viewEnquiries(){
+
+    }
+
+    public void replyEnquiries(){
+
+    }
+
+
+
+
 }
