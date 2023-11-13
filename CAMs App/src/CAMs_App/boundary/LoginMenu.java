fbf648dd7598454b9c7e1a43381b348d365252a9
WJ-MJ <115694501+WJ-MJ@users.noreply.github.com>
2023-11-13 22:02:17 +0800

    update menu
    added new menu and test cases for testing

--- CAMs App/src/CAMs_App/boundary/LoginMenu.java
+++ CAMs App/src/CAMs_App/boundary/LoginMenu.java
@@ -1,11 +1,91 @@
 package CAMs_App.boundary;
 
+import java.security.cert.TrustAnchor;
+import java.util.Scanner;
+
+import CAMs_App.service.AuthStaffService;
+import CAMs_App.service.AuthStudentService;
+import CAMs_App.service.HelperService;
+
 public class LoginMenu implements Menu {
     public void printMenu(){
+        HelperService.clearScreen();
+        System.out.println("Login as:");
+        System.out.println("(1) Staff");
+        System.out.println("(2) Student");
+        System.out.println("(3) Exit\n");
         
     }
 
     public void viewApp(){
+        this.printMenu();
+        Scanner sc = new Scanner(System.in);
+        int choice;
+        boolean authenticated = false;
+        String userID, password;
 
+        do{
+            System.out.print("Enter ur selection: ");
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    AuthStaffService authStaffService = new AuthStaffService();
+                    sc.nextLine();
+                    System.out.print("User ID: ");
+                    userID = sc.nextLine();
+                    System.out.print("Password: ");
+                    password = sc.nextLine();
+                    authenticated = authStaffService.login(userID, password);
+
+                    while(authenticated != true){               
+                        System.out.println("Wrong UserID or Password, please enter again...");
+                        System.out.print("User ID: ");
+                        userID = sc.nextLine();
+                        System.out.print("Password: ");
+                        password = sc.nextLine();
+                        authenticated = authStaffService.login(userID, password);
+                    }
+
+                    System.out.println("Login success");
+                    StaffMenu staffMenu = new StaffMenu();
+                    staffMenu.viewApp();
+                    
+                    break;
+
+                case 2:
+                    AuthStudentService authStudentService = new AuthStudentService();
+                    sc.nextLine();
+                    System.out.print("User ID: ");
+                    userID = sc.nextLine();
+                    System.out.print("Password: ");
+                    password = sc.nextLine();
+                    authenticated = authStudentService.login(userID, password);
+
+                    while(authenticated != true){               
+                        System.out.println("Wrong UserID or Password, please enter again...");
+                        System.out.print("User ID: ");
+                        userID = sc.nextLine();
+                        System.out.print("Password: ");
+                        password = sc.nextLine();
+                        authenticated = authStudentService.login(userID, password);
+                    }
+
+                    System.out.println("Login success");
+                    break;
+                
+                case 3:
+                    System.out.println("Prompt to Exit...");
+                    System.exit(0);
+                    break;
+                
+                default:
+                    System.out.println("Invalid selection, please select again...");
+                    break;
+            }
+
+        }while(choice != 3);
+    
+        sc.close();
     }
 }
--- CAMs App/src/CAMs_App/boundary/StaffMenu.java
+++ CAMs App/src/CAMs_App/boundary/StaffMenu.java
@@ -1,43 +1,26 @@
 package CAMs_App.boundary;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
+import CAMs_App.data.AuthData;
 import CAMs_App.data.Database;
-import CAMs_App.entity.Staff;
+import CAMs_App.entity.User;
 import CAMs_App.service.HelperService;
 
+public class StaffMenu implements Menu{
+    User currentStaff = AuthData.getCurrentUser();
 
-import CAMs_App.enums.*;
-
-public class MainMenu implements Menu{
-    
     public void printMenu(){
         HelperService.clearScreen();
-        System.out.println("Welcome to CAMs !!!");
+        System.out.println("Welcome back"+ currentStaff.getUserID() + "...");
         System.out.println("(1) Login");
         System.out.println("(2) Change password");
         System.out.println("(3) Exit\n");
     }
 
-    public void testData(){
-        Map<String, Staff> user = new HashMap<String, Staff>();
-        Staff staff1 = new Staff("WWW111", null, Faculty.SCSE);
-        Staff staff2 = new Staff("RRR222", null, Faculty.EEE);
-        Staff staff3 = new Staff("TTT444", null, Faculty.SCSE);
-        Staff staff4 = new Staff("YYY555", null, Faculty.CCEB);
-
-        user.put("WWW111", staff1);
-        user.put("RRR222", staff2);
-        user.put("TTT444", staff3);
-        user.put("YYY555", staff4);
-        
-        Database.setstaffData(user);
-    }
+    
 
     public void viewApp(){
-        this.testData();
         this.printMenu();
         Scanner sc = new Scanner(System.in);
         int choice;
@@ -48,8 +31,7 @@ public class MainMenu implements Menu{
             switch (choice) {
                 case 1:
                     System.out.println("Prompt to Login...");
-                    LoginMenu login = new LoginMenu();
-                    login.viewApp();
+                    
                     break;
 
                 case 2:
