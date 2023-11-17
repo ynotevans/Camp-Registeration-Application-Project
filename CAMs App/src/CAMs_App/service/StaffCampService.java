1b9b4f9f03fa6d5872f0bfaeb31f94cc84118501
Tony <123456789@user.noreply.gmail.com>
2023-11-18 00:02:27 +0800

    removed approve suggestion

--- CAMs App/src/CAMs_App/service/StaffCampService.java
+++ CAMs App/src/CAMs_App/service/StaffCampService.java
@@ -143,12 +143,6 @@ public class StaffCampService extends CampManagementService{
         camp.getSuggestionList(); 
     }
 
-    public void approveSuggestions(Camp camp, Suggestions suggestions){
-        System.out.println("Approve suggestion?");
-        boolean approve = sc.nextBoolean();
-        suggestions.setAccepted(approve);
-    }
-
     // generate attendance report
     public void generateAttendanceReport(Camp camp){
         System.out.println("Participants attending camp");
