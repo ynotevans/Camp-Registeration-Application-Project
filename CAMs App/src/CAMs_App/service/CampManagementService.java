b9f8878910000f4f4ce823aa985ebc93d4cf30e8
LMeng18 <121382376+LMeng18@users.noreply.github.com>
2023-11-18 23:12:34 +0800

    completed campMangementService

--- CAMs App/src/CAMs_App/service/CampManagementService.java
+++ CAMs App/src/CAMs_App/service/CampManagementService.java
@@ -1,6 +1,26 @@
 package CAMs_App.service;
+import java.util.ArrayList;
 
+import CAMs_App.entity.Camp;
+import CAMs_App.entity.Student;
 public class CampManagementService {
-    public void viewAttendeesList(){}
-    public void viewCommitteeList(){}
+    public static void viewAttendeesList(String campName){
+        Camp camp = DatabaseService.getCamp(campName);
+        ArrayList<Student> attendees = camp.getAttendees();
+        System.out.println(campName + "attendees list: ");
+        for(int i = 0 ; i < camp.getAttendees().size() ; i++){
+            System.out.print("User ID: " + attendees.get(i).getUserID() + "    ");
+            System.out.println("Faculty: " + attendees.get(i).getFaculty().toString());
+        }
+        
+    }
+    public static void viewCommitteeList(String campName){
+        Camp camp = DatabaseService.getCamp(campName);
+        ArrayList<Student> committees = camp.getCommittee();
+        System.out.println(campName + "committee list: ");
+        for(int i = 0 ; i < camp.getAttendees().size() ; i++){
+            System.out.print("User ID: " + committees.get(i).getUserID() + "    ");
+            System.out.println("Faculty: " + committees.get(i).getFaculty().toString());
+        }   
+    }
 }
