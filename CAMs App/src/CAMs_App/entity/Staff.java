704ebba5b61412600bd7ea78a61c90c04e45309b
LMeng18 <121382376+LMeng18@users.noreply.github.com>
2023-11-24 13:48:32 +0800

    removed unnecessary methods

--- CAMs App/src/CAMs_App/entity/Staff.java
+++ CAMs App/src/CAMs_App/entity/Staff.java
@@ -6,39 +6,5 @@ public class Staff extends User{
         super(userID, password, faculty);
         super.setIdentity(Identity.staff);
     }
-    @Override
-    public void setFaculty(Faculty faculty) {
-        // TODO Auto-generated method stub
-        super.setFaculty(faculty);
-    }
-
-    @Override
-    public void setUserID(String userID) {
-        // TODO Auto-generated method stub
-        super.setUserID(userID);
-    }
-
-    @Override
-    public String getFaculty() {
-        // TODO Auto-generated method stub
-        return super.getFaculty();
-    }
-
-    @Override
-    public String getPassword() {
-        // TODO Auto-generated method stub
-        return super.getPassword();
-    }
-
-    @Override
-    public Identity getIdentity() {
-        // TODO Auto-generated method stub
-        return super.getIdentity();
-    }
-
-    @Override
-    public String getUserID() {
-        // TODO Auto-generated method stub
-        return super.getUserID();
-    }
+   
 }
