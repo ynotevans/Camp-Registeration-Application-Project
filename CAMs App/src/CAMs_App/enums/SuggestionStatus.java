74f4dc675f264dfe03aeb85cb7d50aac8e5fea7e
wanmis <wanmuhdismail@gmail.com>
2023-11-26 06:31:24 +0800

    added javadoc for suggestionstatus.java

--- CAMs App/src/CAMs_App/enums/SuggestionStatus.java
+++ CAMs App/src/CAMs_App/enums/SuggestionStatus.java
@@ -1,5 +1,20 @@
 package CAMs_App.enums;
 
+/**
+ * The {@link SuggestionsStatus} enum represents the suggestions status in the system.
+ * The {@link Suggestions} uses the following enumerations of {@link SuggestionsStatus}.
+ */
 public enum SuggestionStatus {
-    NEW,PROCESSING,PROCESSED
+    /**
+     * Represents a new {@link Suggestions}.
+     */
+    NEW,
+    /**
+     * Represents a {@link Suggestions} that is processing.
+     */
+    PROCESSING,
+    /**
+     * Represents a {@link Suggestions} that has been processed.
+     */
+    PROCESSED
 }
