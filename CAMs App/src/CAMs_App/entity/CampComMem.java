2340b723ea94aaa25c50dd1cd3a80c260a557b4c
WJ-MJ <115694501+WJ-MJ@users.noreply.github.com>
2023-11-25 18:06:36 +0800

    rename camp com

---
+++ CAMs App/src/CAMs_App/entity/CampComMem.java
@@ -0,0 +1,34 @@
+package CAMs_App.entity;
+
+import java.util.*;
+
+public class CampComMem {
+	private ArrayList<Suggestions> suggestions;
+	private String position;
+
+	public CampComMem(String postion){
+		this.position = postion;
+	};
+	
+	public CampComMem(){}
+	
+	
+	
+	public ArrayList<Suggestions> getSuggestion() {
+		return suggestions;
+	}
+
+	public void setSuggestion(Suggestions s) {
+		this.suggestions.add(s);
+	}
+    
+    public String getPosition() {
+    	return position;
+    }
+    
+    public void setPosition(String position) {
+    	this.position = position;
+    }
+
+
+}
