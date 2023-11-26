facbededcd8676b9c94273d6b81ec63307678127
wanmis <wanmuhdismail@gmail.com>
2023-11-26 10:33:17 +0800

    added authstaffservice.java

--- CAMs App/src/CAMs_App/service/AuthStaffService.java
+++ CAMs App/src/CAMs_App/service/AuthStaffService.java
@@ -5,10 +5,21 @@ import java.util.Map;
 import CAMs_App.data.AuthData;
 import CAMs_App.data.Database;
 import CAMs_App.entity.Staff;
-
+/**
+ * The class {@link AuthStaffService} extends from the abstract class {@link AuthService}. 
+ * It authenticates serivces applicable to staff users. The class contains AuthStaffService constructor and login method.
+ */
 public class AuthStaffService extends AuthService {
+    /**
+     * A {@link AuthStaffService} constructor, to create an instance of authentication service of staff users.
+     */
     public AuthStaffService(){};
-
+    /**
+     * Logs in the staff user with userID and password
+     * @param userID : The userID of the staff user
+     * @param password : The associated password to the userID
+     * @return {@code true}, if the login is successful, {@code false} otherwise
+     */
     public boolean login(String userID, String password){
         Map<String, Staff> staffData = Database.getStaffData();
 
