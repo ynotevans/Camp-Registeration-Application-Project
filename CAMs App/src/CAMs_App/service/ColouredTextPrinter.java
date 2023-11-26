5231a0fc01a8b8b9b2d9e0ed3d915b7d9d28fd68
wanmis <wanmuhdismail@gmail.com>
2023-11-26 12:20:08 +0800

    added javadoc for colouredtextprinter.java

--- CAMs App/src/CAMs_App/service/ColouredTextPrinter.java
+++ CAMs App/src/CAMs_App/service/ColouredTextPrinter.java
@@ -1,5 +1,8 @@
 package CAMs_App.service;
-
+/**
+ * The class {@link ColouredTextPrinter} prints color text in the console using ANSI escape codes.
+ * The class includes methods for printing text of various colors and a rainbow effect for each color
+ */
 public class ColouredTextPrinter {
         // ANSI escape codes for colors
         static final String RESET = "\u001B[0m";
@@ -18,36 +21,60 @@ public class ColouredTextPrinter {
             "\u001B[34m", // Blue
             "\u001B[35m"  // Purple
     };
-       
+        /**
+         * Prints the text in red
+         * @param text : The text content to be printed
+         */
         public static void printRed(String text) {
             System.out.println(RED + text + RESET);
         }
-    
+        /**
+         * Prints the text in green
+         * @param text : The text content to be printed
+         */
         public static void printGreen(String text) {
             System.out.println(GREEN + text + RESET);
         }
-    
+        /**
+         * Prints the text in yellow
+         * @param text : The text content to be printed
+         */
         public static void printYellow(String text) {
             System.out.println(YELLOW + text + RESET);
         }
-    
+        /**
+         * Prints the text in blue
+         * @param text : The text content to be printed
+         */
         public static void printBlue(String text) {
             System.out.println(BLUE + text + RESET);
         }
-    
+        /**
+         * Prints the text in purple
+         * @param text : The text content to be printed
+         */
         public static void printPurple(String text) {
             System.out.println(PURPLE + text + RESET);
         }
-    
+        /**
+         * Prints the text in cyan
+         * @param text : The text content to be printed
+         */
         public static void printCyan(String text) {
             System.out.println(CYAN + text + RESET);
         }
-    
+        /**
+         * Prints the text in white
+         * @param text : The text content to be printed
+         */
         public static void printWhite(String text) {
             System.out.println(WHITE + text + RESET);
         }
 
-   
+       /**
+         * Prints the text in rainbow effect, different color for each character
+         * @param text : The text content to be printed
+         */
         public static void printRainbowText(String text) {
             for (int i = 0; i < text.length(); i++) {
                 char currentChar = text.charAt(i);
