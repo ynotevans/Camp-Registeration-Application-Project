e0350f5cba4f1cc6508df3c2113288d547aca1f1
wanmis <wanmuhdismail@gmail.com>
2023-11-26 10:32:57 +0800

    added java doc to authstudentservice.java

--- CAMs App/src/CAMs_App/service/AuthStudentService.java
+++ CAMs App/src/CAMs_App/service/AuthStudentService.java
@@ -4,11 +4,21 @@ import CAMs_App.entity.Student;
 import CAMs_App.data.AuthData;
 import CAMs_App.data.Database;
 
-
+/**
+ * The class {@link AuthStudentService} extends from the abstract class {@link AuthService}. 
+ * It authenticates serivces applicable to student users. The class contains AuthStudentService constructor and login method.
+ */
 public class AuthStudentService extends AuthService {
-   
+    /**
+     * A {@link AuthStudentService} constructor, to create an instance of authentication service of student users.
+     */
     public AuthStudentService(){};
-
+    /**
+     * Logs in the student user with userID and password
+     * @param userID : The userID of the student user
+     * @param password : The associated password to the userID
+     * @return {@code true}, if the login is successful, {@code false} otherwise
+     */
     public boolean login(String userID, String password){
         Map<String, Student> studentData = Database.getStudentsData();
 
