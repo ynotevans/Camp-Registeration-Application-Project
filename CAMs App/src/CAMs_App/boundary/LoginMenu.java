9dfc6e00537e9c6b7599a98eedd30f1ccff75dca
wanmis <wanmuhdismail@gmail.com>
2023-11-26 08:10:14 +0800

    Added javadoc for LoginMenu.java

--- CAMs App/src/CAMs_App/boundary/LoginMenu.java
+++ CAMs App/src/CAMs_App/boundary/LoginMenu.java
@@ -7,8 +7,14 @@ import CAMs_App.data.AuthData;
 import CAMs_App.entity.User;
 import CAMs_App.service.ColouredTextPrinter;
 import CAMs_App.service.HelperService;
-
+/**
+ * The {@link LoginMenu} implements the generic menu, {@link Menu}.
+ * It provides methods to print the main menu and viewing the application 
+ */
 public class LoginMenu implements Menu {
+    /**
+     * Prints the login menu with options for Staff or Student, and option to quit.
+     */
     public void printMenu(){
         HelperService.clearScreen();
         HelperService.printRoute("Login Menu");
@@ -19,7 +25,18 @@ public class LoginMenu implements Menu {
         
     }
 
-    
+    /**
+     * Displays the login menu and reads the user input for role selection or exitting program. 
+     * This method uses a switch case statements to handle the user inputs.
+     * <p><ul>
+     * <li>1 - Staff login: Initiates staff login and navigates to the {@link StaffMenu}.
+     * <li>2 - Student login: Initiates student login and navigates to the {@link StudentMenu}
+     * <li>3 - Exit program: Ends the session and terminates the program.
+     * <li>Invalid selections prompt the user to choose again.
+     * </ul><p>
+     * Users are given total of 5 attempts for login.
+     * If the user is logged in for the first time, the user will be prompted to change password.
+     */
     public void viewApp(){
         this.printMenu();
         Scanner sc = new Scanner(System.in);
