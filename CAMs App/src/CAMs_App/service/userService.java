43387537dd9ec057614ad4ab94075a546fe5555b
wanmis <wanmuhdismail@gmail.com>
2023-11-26 11:05:52 +0800

    added javadoc for UserService.java

--- CAMs App/src/CAMs_App/service/userService.java
+++ CAMs App/src/CAMs_App/service/userService.java
@@ -1,9 +1,16 @@
 package CAMs_App.service;
 import CAMs_App.entity.User;
 import CAMs_App.data.AuthData;
-
+/**
+ * The class {@link UserService} provides method to change user's password
+ */
 public class UserService {
-
+    /**
+     * Changes the user's password
+     * @param oldPassword : The old password to be checked
+     * @param newPassword : The new password to be set if the old password is correct
+     * @return {@code true} if change of password is successful, {@code false} otherwise
+     */
     public static boolean changePassword(String oldPassword, String newPassword){
         User user = AuthData.getCurrentUser();
         if (!oldPassword.equals(user.getPassword()))
