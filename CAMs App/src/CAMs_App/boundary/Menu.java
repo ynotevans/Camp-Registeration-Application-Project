8889b84a2ba27885bd9df1dca5e62ca1c62b5df1
wanmis <wanmuhdismail@gmail.com>
2023-11-26 08:05:49 +0800

    added javadoc for Menu.java

--- CAMs App/src/CAMs_App/boundary/Menu.java
+++ CAMs App/src/CAMs_App/boundary/Menu.java
@@ -1,11 +1,17 @@
 package CAMs_App.boundary;
 
+/**
+ * The {@link Menu} interface represents the generic menu that
+ * defines printMenu() and viewApp() functions
+ */
 public interface Menu {
-    
-    //print the available selections for the certain class
+    /**
+     * Prints the menu and its available selections for the specific class.
+     */
     public void printMenu();
-
-    //call the printMenu(), prompt user input and call the respective menu based on user input
+    /**
+     * Call the printMenu(), prompt user input and calls the respective menu based on user input
+     */
     public void viewApp();
     
 }
