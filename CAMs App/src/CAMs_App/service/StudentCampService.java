af1223c8965df480edac0e7a0d7485c5cf17d92b
wanmis <wanmuhdismail@gmail.com>
2023-11-26 11:58:22 +0800

    added javadoc for studentcampservice.java

--- CAMs App/src/CAMs_App/service/StudentCampService.java
+++ CAMs App/src/CAMs_App/service/StudentCampService.java
@@ -8,9 +8,16 @@ import CAMs_App.data.AuthData;
 import CAMs_App.entity.Camp;
 import CAMs_App.entity.Student;
 import CAMs_App.enums.Faculty;
-
+/**
+ * The class {@link StudentCampService} is responsible in managing the camp-related functionalities for student users
+ */
 public class StudentCampService {
-	
+	/**
+	 * Checks where the two camps clash/overlaps in terms of date
+	 * @param camp1 : The first camp
+	 * @param camp2 : The second camp
+	 * @return {@code true} if the dates do not clash,{@code false } dates do clash
+	 */
 	private static boolean checkClash(Camp camp1 , Camp camp2){
 		LocalDate camp1Start = camp1.getCampDate();
 		LocalDate camp1End  = camp1Start.plusDays(camp1.getNumberOfCampDays());
@@ -21,7 +28,10 @@ public class StudentCampService {
 		boolean isClash = !((camp1End.isBefore(camp2Start)) || (camp1Start.isAfter(camp2End)));
     return isClash;
 	}
-
+	/**
+	 * Checks if the camp to be registered clashes with the camps that already has been registered.
+	 * @return {@code true} if there is a date clash, {@code false} otherwise
+	 */
 	public static boolean checkCampClashDate(){
 		Student student = (Student)AuthData.getCurrentUser();
 		Camp newCamp = AuthData.getCurrentCamp(); //to check clash date with this camp
@@ -36,25 +46,36 @@ public class StudentCampService {
 		}
 	return false;
 	}
-
+	/**
+	 * Checks if the register deadline for the current camp has passed
+	 * @return {@code true} if the register deadline has not passed, {@code false} otherwise
+	 */
 	public static boolean checkRegisterDeadline(){
 		Camp camp = AuthData.getCurrentCamp();
 		if(camp.getRegCloseDate().isBefore(LocalDate.now()))return true;
 		return false;
 
 	}
-
+	/**
+	 * Check is the current camp capacity is full
+	 * @return {@code true} if the camp has no more slots,{@code false} otherwise
+	 */
 	public static boolean isCampFull(){
 		Camp camp = AuthData.getCurrentCamp();
 		if(camp.getRemainingSlot() == 0) return true;
 		return false;
 	}
-
+	/**
+	 * Updates the current camp's remaining slots after a student registers for the camp
+	 */
 	public static void updateRemainingSlot(){
 		Camp camp = AuthData.getCurrentCamp();
 		camp.setRemainingSlot(camp.getRemainingSlot()-1);
 	}
-
+	/**
+	 * Checks if the student has withdrawn from the camp before
+	 * @return {@code true} if the student has withdrawn from the camp, {@code false} otherwise
+	 */
 	public static boolean checkWithdrawBefore(){
 		Student user = (Student)AuthData.getCurrentUser();
 		ArrayList<String> withdraw = user.getWithdrawnCamp();
@@ -65,7 +86,9 @@ public class StudentCampService {
 		}
 		return false;
 	}
-
+	/**
+	 * The student user register the current camp as an attendee
+	 */
 	public static void registerAsAttendee(){
 		Student user = (Student)AuthData.getCurrentUser();
 		Camp camp = AuthData.getCurrentCamp();
@@ -75,7 +98,9 @@ public class StudentCampService {
 		camp.setRemainingSlot(camp.getRemainingSlot()-1);
 		
 	}
-	
+	/**
+	 * The student user register the current camp as a camp committee member fo that camp
+	 */
 	public static void registerAsCommittee(String position){
 		Student student = (Student)AuthData.getCurrentUser();
 		Camp camp = AuthData.getCurrentCamp();
@@ -87,7 +112,10 @@ public class StudentCampService {
 		student.setCommitteeCamp(camp);
 		camp.setCampCommitteeRemainingSlots(camp.getCampCommitteeRemainingSlots()-1);
 	}
-
+	/**
+	 * View the available camps to join based on the visibility and faculty
+	 * @return the number of available camps
+	 */
 	// view available camp
     public static int viewAvailableCamps(){
 		int i=0;
@@ -104,7 +132,10 @@ public class StudentCampService {
         }
 		return i;
     }
-
+	/**
+	 * View the camps registered by the current Student user
+	 * @ return the number of registered camps
+	 */
 	public static int viewRegisteredCamp(){
 		Student user = (Student)AuthData.getCurrentUser();
 		int i = 0;
@@ -122,7 +153,12 @@ public class StudentCampService {
 		return i;
     }
 
-
+	/**
+	 * Student user withdraws from the current camp
+	 * @param campName : Name of camp to be withdrawn
+	 * @param student : Current student
+	 * @param isComittee : Whether the student is a camp committee member
+	 */
 	public static void withdrawCamp(String campName, Student student, boolean isComittee){
 		Camp camp = DatabaseService.getCamp(campName);
 		// Student user = (Student)AuthData.getCurrentUser();
